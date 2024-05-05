b15a129e2a5b4e9653dec19eeee22d19ad0afa17
Pamuditha <wickramasinha1219@gmail.com>
2024-05-05 18:46:47 +0530

    create SEO

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -14,11 +14,28 @@ class MovieController extends Controller
         try {
             $movie = Movie::with('categories', 'top_casts', 'directors', 'formats', 'languages')->findOrFail($id);
             $languages = Language::all();
-            $latestMovies = Movie::orderBy('created_at', 'desc')
-                                ->take(4)
-                                ->get();
-                                
-            return view('movie.index', compact('movie', 'latestMovies', 'languages'));
+            $latestMovies = Movie::latest()->limit(4)->get();
+                    
+            $metaKeywords = '';
+            foreach ($languages as $language) {
+                $metaKeywords .= ', ' . $language->language;
+            }
+
+            foreach ($movie->categories as $category) {
+                $metaKeywords .= ', ' . $category->category;
+            }
+
+            foreach ($movie->top_casts as $top_cast) {
+                $metaKeywords .= ', ' . $top_cast->name;
+            }
+
+            foreach ($movie->directors as $director) {
+                $metaKeywords .= ', ' . $director->name;
+            }
+            
+            $metaKeywords .= ', ' . $movie->title;
+            
+            return view('movie.index', compact('movie', 'latestMovies', 'languages', 'metaKeywords'));
         } catch (Exception $e) {
             return response()->json(['message' => 'MovieController >> index >> Failed to get movies: ' . $e->getMessage()], 500);
         }
--- resources/views/movie/index.blade.php
+++ resources/views/movie/index.blade.php
@@ -1,16 +1,20 @@
 @extends('layout.secound')
 
-@section('title')
-{{ $movie->title }} - Watch on Asset Movie
-@endsection
+@section('title', $movie->title . ' - Watch on Asset Movie And Download Torrents Movies')
 
-@section('meta_description')
-{{ $movie->description }}
-@endsection
+@section('meta_description', $movie->description . 'Asset Movie offers an extensive library of movies spanning multiple
+genres and languages.
+Discover blockbuster hits, indie gems, and cult classics. Dive into detailed movie descriptions, watch trailers, and
+download torrents directly. Whether you are a cinephile or casual viewer, Asset Movie is your go-to destination for
+cinematic exploration. Discover a world of movies on Asset Movie. Browse films from various countries and
+languages, and download torrents for your favorite picks.')
 
-@section('meta_keywords')
-movie download, watch {{ $movie->title }}
-@endsection
+@section('meta_keywords', 'watch' . $movie->title . 'download' . $movie->title . 'torrents download' . $movie->title .
+$metaKeywords . ',international cinema,
+Asset Movie, torrents
+downloads, free movies,
+streaming, new releases, Hollywood, Bollywood, action, comedy, drama, thriller, romance, sci-fi, horror, documentary,
+foreign films')
 
 @section('styles')
 <link rel="stylesheet" href="{{ asset('assets/css/movie.css') }}">
