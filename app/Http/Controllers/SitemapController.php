860d2b22d38a66b37b9cdd4b046b576a881aefaf
Pamuditha <wickramasinha1219@gmail.com>
2024-06-19 23:00:25 +0530

    update sitemap

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -24,6 +24,34 @@ class SitemapController extends Controller
         );
 
         $sitemap->add(
+            Url::create($domain . "/privacy-policy")
+                ->setLastModificationDate(Carbon::yesterday())
+                ->setChangeFrequency(Url::CHANGE_FREQUENCY_NEVER)
+                ->setPriority(0.8)
+        );
+
+        $sitemap->add(
+            Url::create($domain . "/terms-of-service")
+                ->setLastModificationDate(Carbon::yesterday())
+                ->setChangeFrequency(Url::CHANGE_FREQUENCY_NEVER)
+                ->setPriority(0.8)
+        );
+
+        $sitemap->add(
+            Url::create($domain . "/about")
+                ->setLastModificationDate(Carbon::yesterday())
+                ->setChangeFrequency(Url::CHANGE_FREQUENCY_NEVER)
+                ->setPriority(0.8)
+        );
+
+        $sitemap->add(
+            Url::create($domain . "/contact")
+                ->setLastModificationDate(Carbon::yesterday())
+                ->setChangeFrequency(Url::CHANGE_FREQUENCY_NEVER)
+                ->setPriority(0.8)
+        );
+
+        $sitemap->add(
             Url::create($domain . "/movie/latest")
                 ->setLastModificationDate(Carbon::yesterday())
                 ->setChangeFrequency(Url::CHANGE_FREQUENCY_NEVER)
@@ -43,7 +71,7 @@ class SitemapController extends Controller
         $languages = Language::all();
         foreach ($languages as $language) {
             $sitemap->add(
-                Url::create($domain . "/movie/latest?id={$language->id}")
+                Url::create($domain . "/movie/language?id={$language->id}")
                     ->setLastModificationDate($language->updated_at)
                     ->setChangeFrequency(Url::CHANGE_FREQUENCY_YEARLY)
                     ->setPriority(0.8)
@@ -68,18 +96,18 @@ $xml .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" ';
         $xml .= ' xmlns:news="http://www.google.com/schemas/sitemap-news/0.9">' . PHP_EOL;
 
     foreach ($sitemap->getTags() as $tag) {
-    $xml .= '	<url>' . PHP_EOL;
-        $xml .= '		<loc>' . htmlspecialchars($tag->url) . '</loc>' . PHP_EOL;
+    $xml .= ' <url>' . PHP_EOL;
+        $xml .= ' <loc>' . htmlspecialchars($tag->url) . '</loc>' . PHP_EOL;
         if ($tag->lastModificationDate) {
-        $xml .= '		<lastmod>' . $tag->lastModificationDate->toAtomString() . '</lastmod>' . PHP_EOL;
+        $xml .= ' <lastmod>' . $tag->lastModificationDate->toAtomString() . '</lastmod>' . PHP_EOL;
         }
         if ($tag->changeFrequency) {
-        $xml .= '		<changefreq>' . $tag->changeFrequency . '</changefreq>' . PHP_EOL;
+        $xml .= ' <changefreq>' . $tag->changeFrequency . '</changefreq>' . PHP_EOL;
         }
         if ($tag->priority) {
-        $xml .= '		<priority>' . $tag->priority . '</priority>' . PHP_EOL;
+        $xml .= ' <priority>' . $tag->priority . '</priority>' . PHP_EOL;
         }
-        $xml .= '	</url>' . PHP_EOL;
+        $xml .= ' </url>' . PHP_EOL;
     }
 
     $xml .= '</urlset>';
