28b9434da1a1d9c7567a63bcbf098ed8a8f93e37
Pamuditha <wickramasinha1219@gmail.com>
2024-06-24 01:27:53 +0530

    FIX | js file issue

--- resources/js/movie.js
+++ resources/js/movie.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+    var interval;
+
     $(document).on('click', '.download-link', function(event) {
         event.preventDefault();
         var formatId = $(this).data('format-id');
@@ -6,36 +8,52 @@ $(document).ready(function() {
 
         $('#download-button').attr('href', '/movie/download/' + formatId + '/' + movieId);
         $('#downloadModal').show();
+        startLoading();
+    });
+
+    $('.close').on('click', function() {
+        $('#downloadModal').hide();
+        clearInterval(interval);
+        resetLoading();
+    });
+
+    $(window).on('click', function(event) {
+        if ($(event.target).is('#downloadModal')) {
+            $('#downloadModal').hide();
+            clearInterval(interval);
+            resetLoading();
+        }
+    });
+
+    $('#download-button').on('click', function() {
+        $('#downloadModal').hide();
+        clearInterval(interval); 
+        resetLoading();
+    });
+
+    function startLoading() {
         var progress = 1;
-        var interval = setInterval(function() {
+        interval = setInterval(function() {
             if (progress >= 100) {
                 clearInterval(interval);
-                $('#status').text('File Loading ...');
+                $('#status').text('File Loading...');
                 setTimeout(function() {
-                    $('#status').text('File loaded successfully !');
+                    $('#status').text('File loaded successfully!');
                     $('#download-button').show();
                 }, 500);
             } else {
                 progress++;
                 $('#progress-bar').css('width', progress + '%').text(progress + '%');
                 if (progress === 50) {
-                    $('#status').text('File Loading ...');
+                    $('#status').text('File Loading...');
                 }
             }
         }, 100);
-    });
+    }
 
-    $('.close').on('click', function() {
-        $('#downloadModal').hide();
-    });
-
-    $(window).on('click', function(event) {
-        if ($(event.target).is('#downloadModal')) {
-            $('#downloadModal').hide();
-        }
-    });
-
-    $('#download-button').on('click', function() {
-        $('#downloadModal').hide();
-    });
+    function resetLoading() {
+        $('#progress-bar').css('width', '1%').text('1%');
+        $('#status').text('Connecting to server...');
+        $('#download-button').hide();
+    }
 });
